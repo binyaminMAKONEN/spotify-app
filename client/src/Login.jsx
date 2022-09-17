c8e84119a985b7f8c74c1e54cf1709a550a87a09
benjamin <bnymynmqwnn@gmail.com>
2022-09-18 00:31:05 +0300

    log

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Container} from 'react-bootstrap'
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=50c66722fae74cfa992960850b485c9e&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container className='d-flex justify-content-center align-items-center'
+    style={{minHeight:'94vh'}}>
+     <a className='btn btn-success btn-lg' href={AUTH_URL}>
+        Login wite spotify
+     </a>
+    </Container>
   )
 }
 
