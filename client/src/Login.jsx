5518369f76f4062c3f0f24b8cc36c9a8b7b4ca16
benjamin <bnymynmqwnn@gmail.com>
2022-09-17 22:43:46 +0300

    login

---
+++ client/src/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=50c66722fae74cfa992960850b485c9e&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
