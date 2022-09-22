2bea86b387c049717542964087e0bf87b6db23d8
benjamin <bnymynmqwnn@gmail.com>
2022-09-22 09:09:29 +0300

    login

--- client/src/useAuto.js
+++ client/src/useAuto.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -22,6 +21,24 @@ export default function useAuto(code) {
         console.log(err);
       });
   }, [code])
+
+  useEffect(() => {
+
+    axios
+    .post("http://localhost:3001/refresh", { refreshToken })
+    .then((res) => {
+      console.log(res.data)
+      // setAccessToken(res.data.accessToken);
+      // setRefreshToken(res.data.refreshToken);
+      // setExpiresIn(res.data.expiresIn);
+      // window.history.pushState({},null,"/");
+  })
+    .catch((err) => {
+      window.location = "/";
+      console.log(err);
+    });
+
+  },[refreshToken,expiresIn])
 
   return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -5,12 +5,38 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
 app.use(cors())
 app.use(bodyParser.json())
+
+
+app.post("/refresh",(req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+  clientId: '391d1235afa54e04aa06774a0bbd8e78',
+  clientSecret: '51f6f8b9a495452fb7107b499d418868',
+  refreshToken
+})
+spotifyApi
+.refreshAccessToken()
+.then(data => {
+  console.log(data.body);
+  res.json({
+    accessToken: data.body.accessToken,
+    expiresIn: data.body.expiresIn,
+  })
+})
+.catch(err => {
+  console.log(err)
+  res.sendStatus(400)
+})
+})
+
+
 app.post('/login',(req, res) =>{
     const code=req.body.code;
   const spotifyApi = new SpotifyWebApi({
       redirectUri: 'http://localhost:3000',
-    clientId: '50c66722fae74cfa992960850b485c9e',
-    clientSecret: 'a0f2726ca86146e483d79dbbc0c44f69',
+    clientId: '391d1235afa54e04aa06774a0bbd8e78',
+    clientSecret: '51f6f8b9a495452fb7107b499d418868',
   
   })
   spotifyApi.authorizationCodeGrant(code).then(data=>{
@@ -20,6 +46,7 @@ app.post('/login',(req, res) =>{
         expiresIn:data.body.expires_in
     })
   }).catch((err)=>{ 
+    console.log('nvvvv');
     console.log("this:" ,err);
     res.sendStatus(400)})
 })
