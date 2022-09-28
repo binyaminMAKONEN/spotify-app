f90be5652bff2b3182f2f6839a76a816e3ebea16
benjamin <bnymynmqwnn@gmail.com>
2022-09-28 19:05:46 +0300

    start over

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Login from './Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Dashboard from './Dashboard';
 
-const code = new URLSearchParams(window.location.search).get('code'); 
+
 function App() {
-  return code ? <Dashboard code={code}/> : <Login/>
+  return (
+    <div>App</div>
+  )
   
 }
 
