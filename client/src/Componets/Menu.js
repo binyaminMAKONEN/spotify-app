6cece0b75dbf778de60880f68afd46c003bae269
benjamin <bnymynmqwnn@gmail.com>
2022-10-01 23:46:25 +0300

    left menu

--- client/src/Componets/Menu.js
+++ client/src/Componets/Menu.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 function Menu({title,menuObject}) {
+
+    useEffect(()=>{
+    },[])
   return (
     <div className='menuContainer'>
         <p className='title'>{title}</p>
