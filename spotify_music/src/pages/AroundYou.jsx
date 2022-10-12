75ab8c2f4409cf481cf19b85b96512192fc4aa74
benjamin <bnymynmqwnn@gmail.com>
2022-10-12 20:11:12 +0300

    new-progect

--- spotify_music/src/pages/AroundYou.jsx
+++ spotify_music/src/pages/AroundYou.jsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
-export default CountryTracks;
+const AroundYou = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+console.log(data);
+  useEffect(()=>{
+  //at_Y7adZa95oUko2yMTs6ucl3wO7mDtY
+  axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_Y7adZa95oUko2yMTs6ucl3wO7mDtY`)
+  .then((res)=>setCountry(res?.data?.location.country))
+  .catch((res)=>console.log(res))
+  .finally(()=>setLoading(false))
+  },[country])
+
+  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+  if (error && country !== '') return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around you <span className="font-black">{country}</span></h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AroundYou;
--- spotify_music/src/pages/Discover.jsx
+++ spotify_music/src/pages/Discover.jsx
@@ -2,22 +2,26 @@ import { useDispatch,useSelector } from "react-redux";
 import { Error,Loader,SongCard } from "../components";
 import {genres} from '../assets/constants'
 import {useGetTopChartsQuery} from '../redux/services/shazamCore'
+import {useGetSongsByGenreQuery} from '../redux/services/shazamCore'
+import { selectGenreListId } from "../redux/features/playerSlice";
 
 const Discover = () =>{ 
-    const {data,isFetching,error}=useGetTopChartsQuery()
     const dispatch = useDispatch()
+    const { genreListId } = useSelector((state) => state.player);
     const {activeSong,isPlaying} = useSelector((state)=> state.player)
+    const {data,isFetching,error}=useGetSongsByGenreQuery(genreListId || "POP" )
 
     if(isFetching)return <Loader title='Loading songs...'/>;
     if(error)return <Error/>;
     
+    const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
     return(
      <div className="flex flex-col">
         <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-            <h2 className='font-bold text-3xl text-white text-left'>Discover</h2>
+            <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
             <select
-            onChange={()=>{}}
-            value=''
+            onChange={(e)=>{dispatch(selectGenreListId(e.target.value))}}
+            value={genreListId || "POP"}
             className="bg-black text-gray-300
             p-3 text-sm-rounded-lg outline-none sm:mt-0 mt-5"
             >
