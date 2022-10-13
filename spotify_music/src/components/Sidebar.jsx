bc977165163bc32bb4144aa2ff1a4563d7077ee2
benjamin <bnymynmqwnn@gmail.com>
2022-10-13 14:50:27 +0300

    responsive

--- spotify_music/src/components/Sidebar.jsx
+++ spotify_music/src/components/Sidebar.jsx
@@ -27,7 +27,7 @@ const Sidebar = () => {
 //  backdrop-blur-md
   return (
     <>
-      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#181818]">
+      <div className="md:flex hidden flex-col w-[200px] py-10 px-4 bg-[#181818]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
@@ -41,7 +41,7 @@ const Sidebar = () => {
         )}
       </div>
 
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : 'left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
