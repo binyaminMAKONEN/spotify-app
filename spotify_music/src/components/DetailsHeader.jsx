e79d4e966503bd349df468461889cc45c51e359e
benjamin <bnymynmqwnn@gmail.com>
2022-10-13 16:53:45 +0300

    artisDetails

--- spotify_music/src/components/DetailsHeader.jsx
+++ spotify_music/src/components/DetailsHeader.jsx
@@ -3,9 +3,15 @@ import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => {
 const artist= artistData?.artists[artistId]?.attributes;
+console.log(artistData)
   return(
     <div className="relative w-full flex flex-col">
-    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+    <div className="w-full bg-gradient-to-l from-transparent to-[#181818] sm:h-48 h-28"/>
+    {/* <img alt='art'
+      src={artistId ? artist.artwork?.url.replace(`{w}`,'500').replace(`{h}`,'500'):songData?.images?.coverart}
+      className=" w-full sm:h-48 h-full  object-cover border-1 shadow-xl shadow-black md:hidden"
+      />
+    </div> */}
     <div className="absolute inset-0 flex items-center">
       <img alt='art'
       src={artistId ? artist.artwork?.url.replace(`{w}`,'500').replace(`{h}`,'500'):songData?.images?.coverart}
--- spotify_music/src/components/Sidebar.jsx
+++ spotify_music/src/components/Sidebar.jsx
@@ -40,8 +40,8 @@ const Sidebar = () => {
           <RiCloseLine className="w-6 h-6 mr-2 text-white" onClick={() => setMobileMenuOpen(false)} />
         )}
       </div>
-
-      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : 'left-full'}`}>
+{/* bg-gradient-to-tl from-white/10 to-[#483D8B] */}
+      <div className={`absolute top-0 h-screen w-2/3  backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
